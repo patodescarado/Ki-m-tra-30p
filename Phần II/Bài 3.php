119ea5711c1b238929f078b6569d762f230ae6aa
Phạm Đức <dailyroutine2501@gmail.com>
2026-09-07 14:51:58 +0700

    Add files via upload

--- Phần II/Bài 3.php
+++ Phần II/Bài 3.php
@@ -1,1 +1,9 @@
+<?php
+$NamNay = date("Y"); 
 
+if ($NamNay % 2 == 0) {
+    echo "Năm $NamNay là năm chẵn";
+} else {
+    echo "Năm $NamNay là năm lẻ";
+}
+?>
--- Phần II/Bài6.php
+++ Phần II/Bài6.php
@@ -1,1 +1,8 @@
+<?php
+$nam = array(1990, 1991, 1992, 1993, 1994, 1995);
+
+foreach ($nam as $n) {
+    echo $n . "<br>";
+}
+?>
 
